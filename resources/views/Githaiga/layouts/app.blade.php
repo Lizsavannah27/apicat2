e20b8d9a8b088bfa5660aca2d32edfd193cd5118
Lizz <lizz.githaiga@strathmore.edu>
2020-04-15 20:42:41 +0300

    removed asset helper

--- resources/views/Githaiga/layouts/app.blade.php
+++ resources/views/Githaiga/layouts/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/app.css') }}">
-    <script src="{{asset('js/app.js') }}"></script>
+    <link rel="stylesheet" href="css/app.css">
+    <script src="js/app.js"></script>
     <title>API CAT2</title>
 </head>
 <body>
